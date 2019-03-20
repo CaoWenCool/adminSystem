2f3a686a3824406e62ef3c59c0b6d58e1ea5cb9d
caowen <1042879511@qq.com>
2019-03-20 10:38:09 +0800

    添加管理员系统

---
+++ src/main/java/com/demo/adminsystem/AdminsystemApplication.java
@@ -0,0 +1,13 @@
+package com.demo.adminsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminsystemApplication.class, args);
+	}
+
+}
