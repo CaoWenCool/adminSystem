8ac26a674b865d1c05e9ce5eadf23a7b3a131a22
caowen <1042879511@qq.com>
2019-03-20 16:45:21 +0800

    add email plugins

---
+++ src/main/java/com/demo/adminsystem/core/configure/VelocityEngineConfig.java
@@ -0,0 +1,34 @@
+package com.demo.adminsystem.core.configure;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Properties;
+
+/**
+ * @author: admin
+ * @create: 2019/3/20
+ * @update: 16:44
+ * @version: V1.0
+ * @detail: VelocityEngineConfig 模板渲染引擎配置
+ **/
+@Configuration
+public class VelocityEngineConfig {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public VelocityEngine velocityEngine() throws Exception {
+        Properties properties = new Properties();
+        String[] paramsNames = new String[]{"input.encoding", "output.encoding", "resource.loader", "class.resource.loader.class"};
+        for (String name : paramsNames) {
+            properties.setProperty(name, environment.getProperty("velocity.engine." + name));
+        }
+        VelocityEngine velocityEngine = new VelocityEngine(properties);
+        return velocityEngine;
+    }
+}
---
+++ src/main/java/com/demo/adminsystem/core/plugins/email/SendTestEmail.java
@@ -0,0 +1,90 @@
+package com.demo.adminsystem.core.plugins.email;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.StringWriter;
+import java.util.Date;
+
+/**
+ * @author: admin
+ * @create: 2019/3/20
+ * @update: 16:20
+ * @version: V1.0
+ * @detail:
+ **/
+public class SendTestEmail {
+    @Autowired
+    private JavaMailSenderImpl javaMailSender;
+    @Autowired
+    private VelocityEngine velocityEngine;
+
+    /**
+     * 发送文本文件
+     * @param to
+     * @param cc
+     * @param subject
+     * @param content
+     */
+    public void sendTextMail(String[] to,String[] cc,String subject,String content){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(javaMailSender.getUsername());
+        mailMessage.setTo(to);
+        if(cc != null){
+            mailMessage.setCc(cc);
+        }
+        mailMessage.setSubject(subject);
+        mailMessage.setText(content);
+        mailMessage.setSentDate(new Date());
+        javaMailSender.send(mailMessage);
+    }
+
+    public void sendTextMailWithFile(String[] to,String[] cc,String subject,String content,File[] files)throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mailMessage = new MimeMessageHelper(mimeMessage,true);
+        mailMessage.setFrom(javaMailSender.getUsername());
+        mailMessage.setTo(to);
+        if(cc != null){
+            mailMessage.setCc(cc);
+        }
+        mailMessage.setSubject(subject);
+        mailMessage.setText(content);
+        mailMessage.setSentDate(new Date());
+        for(File file:files){
+            mailMessage.addAttachment(file.getName(),file);
+        }
+        javaMailSender.send(mimeMessage);
+    }
+
+    public void sendTemplateMail(String[] to, String[] cc, String subject, String templeName, JSONObject model, File[] files) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mailMessage = new MimeMessageHelper(mimeMessage, true);
+        mailMessage.setFrom(javaMailSender.getUsername());
+        mailMessage.setTo(to);
+        if (cc != null) {
+            mailMessage.setCc(cc);
+        }
+        mailMessage.setSubject(subject);
+
+        VelocityContext context = new VelocityContext();
+        for (String key : model.keySet()) {
+            context.put(key, model.get(key));
+        }
+        StringWriter writer = new StringWriter();
+        velocityEngine.mergeTemplate(templeName, "UTF-8", context, writer);
+        mailMessage.setText(writer.toString());
+        mailMessage.setSentDate(new Date());
+        for (File file : files) {
+            mailMessage.addAttachment(file.getName(), file);
+        }
+        javaMailSender.send(mimeMessage);
+    }
+}
